a4dba8fae8ec8eca2e2fa7a894f8f8448b6dfbbe
Matt Leary <33221468+mleary@users.noreply.github.com>
2022-10-23 22:14:47 -0400

    Update decanter-data.R

--- data/decanter-data.R
+++ data/decanter-data.R
@@ -1,6 +1,6 @@
 # This script defines variables used in index.Rmd
 
  
-HOUSE_BOURBON <- "Very Old Barton"
+HOUSE_BOURBON <- "Very Old Barton (86 Proof)"
 SIPPING_BOURBON <- "Maker's Mark"
 SCOTCH <- "GlenDoranch Distillery (Highland - forgot exact bottle)"
