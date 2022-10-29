792f962b4bee380204a3e1a1fb8c8d3361488494
Matt Leary <33221468+mleary@users.noreply.github.com>
2022-10-28 21:37:58 -0400

    Update decanter-data.R

--- data/decanter-data.R
+++ data/decanter-data.R
@@ -4,4 +4,3 @@
 HOUSE_BOURBON <- "Evan Williams Bottled-in-Bond (White Label)"
 SIPPING_BOURBON <- "Maker's Mark No. 46 (French Oaked)"
 SCOTCH <- "GlenDoranch Distillery (Highland - do not recall exact bottle)"
-
