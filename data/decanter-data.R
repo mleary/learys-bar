166b76abdf2ec9e66914a1d25b1865f451586ecc
Matt Leary <33221468+mleary@users.noreply.github.com>
2022-10-28 21:11:09 -0400

    Update decanter-data.R

--- data/decanter-data.R
+++ data/decanter-data.R
@@ -1,6 +1,7 @@
 # This script defines variables used in index.Rmd
-
+# It is intended to be updated as I refill decanters
 
 HOUSE_BOURBON <- "Evan Williams Bottled-in-Bond (White Label)"
 SIPPING_BOURBON <- "Maker's Mark No. 46 (French Oaked)"
 SCOTCH <- "GlenDoranch Distillery (Highland - do not recall exact bottle)"
+
