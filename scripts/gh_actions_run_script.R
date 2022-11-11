1982d2ec3abc6a02a5c8d44714eda9516799c2fe
Matt Leary <33221468+mleary@users.noreply.github.com>
2022-11-11 17:23:21 -0500

    fix checkpoint path (new for macos)

--- scripts/gh_actions_run_script.R
+++ scripts/gh_actions_run_script.R
@@ -1,6 +1,6 @@
 # Run script used as part of github actions workflow
 
-.libPaths("./.checkpoint/2021-06-01/lib/x86_64-w64-mingw32/4.0.2")
+.libPaths("./.checkpoint/2022-10-01/lib/x86_64-apple-darwin17.0/4.1.2/")
 
 print(getwd()) # test that wd is properly working
 
